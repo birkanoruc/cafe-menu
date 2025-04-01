b44e956a68b2cf216adf353a7d467663418e2f77
Birkan Oruç <birkanoruc@gmail.com>
2025-04-01 14:44:14 +0300

    venuecontroller rescude from service drain

--- app/Http/Controllers/Api/V1/VenueController.php
+++ app/Http/Controllers/Api/V1/VenueController.php
@@ -11,54 +11,116 @@ use App\Http\Requests\Venue\VenueUpdateRequest;
 use App\Models\Venue;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Gate;
+use App\Filters\VenueFilter;
+use App\Exceptions\NotFoundException;
+use App\Http\Resources\VenueCollection;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\VenueResource;
+use Illuminate\Support\Facades\Storage;
+use App\Exceptions\ImageUploadException;
+use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\ProductCollection;
 
 class VenueController extends Controller
 {
-    private VenueServiceInterface $venueService;
-
-    public function __construct(VenueServiceInterface $venueService)
-    {
-        $this->venueService = $venueService;
-    }
-
     public function index(VenueFilterRequest $request): JsonResponse
     {
-        return $this->venueService->getAllVenues($request->validated());
+        $venueFilters = new VenueFilter($request->validated());
+
+        $venues = Venue::query()->where('user_id', Auth::id());
+
+        $filteredVenues = $venueFilters->applyFilters($venues);
+
+        if ($filteredVenues->isEmpty()) {
+            throw new NotFoundException("Mekan bulunamadı!", 404);
+        }
+
+        return response()->json(new VenueCollection($filteredVenues), 200);
     }
 
     public function show(Venue $venue): JsonResponse
     {
         Gate::authorize('view', $venue);
 
-        return $this->venueService->getVenueById($venue->id);
+        $venue->load(['categories', 'products']);
+
+        return response()->json(new VenueResource($venue), 200);
     }
 
     public function store(VenueStoreRequest $request): JsonResponse
     {
-        return $this->venueService->createVenue($request->validated());
+        $venueData = $request->validated();
+
+        if ($request->hasFile('featured_image')) {
+            $venueData['featured_image'] = $this->uploadImage($request->file('featured_image'));
+        }
+
+        $venueData['user_id'] = Auth::id();
+
+        $venue = Venue::create($venueData);
+
+        return response()->json(['message' => 'Mekan başarıyla oluşturuldu!', "venue" => new VenueResource($venue)], 201);
     }
 
     public function update(VenueUpdateRequest $request, Venue $venue): JsonResponse
     {
         Gate::authorize('update', $venue);
 
-        return $this->venueService->updateVenue($venue->id, $request->validated());
+        $venueData = $request->validated();
+
+        if ($request->hasFile('featured_image')) {
+
+            $venue->featured_image && $this->deleteImage($venue->featured_image);
+
+            $venueData['featured_image'] = $this->uploadImage($venueData['featured_image']);
+        }
+
+        $venue->updateOrFail($venueData);
+
+        $venue->load(['categories', 'products']);
+
+        return response()->json(['message' => 'Mekan başarıyla güncellendi!', "venue" => new VenueResource($venue)], 200);
     }
 
     public function destroy(Venue $venue): JsonResponse
     {
         Gate::authorize('delete', $venue);
 
-        return $this->venueService->deleteVenue($venue->id);
+        $venue->featured_image && $this->deleteImage($venue->featured_image);
+
+        $venue->deleteOrFail();
+
+        return response()->json(['message' => 'Mekan başarıyla silindi!'], 200);
     }
 
     public function categories(Venue $venue): JsonResponse
     {
-        return $this->venueService->getCategoriesByVenueId($venue->id);
+        $venue->load(['categories', 'products']);
+
+        return response()->json(new CategoryCollection($venue->categories), 200);
     }
 
     public function products(Venue $venue): JsonResponse
     {
-        return $this->venueService->getProductsByVenueId($venue->id);
+        $products = $venue->products()->with('categories')->get();
+
+        return response()->json(new ProductCollection($products), 200);
+    }
+
+    protected function uploadImage($image): string
+    {
+        $path = $image->store('featured_images/venues', 'public');
+
+        if (!$path) {
+            throw new ImageUploadException();
+        }
+
+        return Storage::url($path);
+    }
+
+    protected function deleteImage(string $imagePath): void
+    {
+        $path = str_replace('/storage/', '', $imagePath);
+        Storage::disk('public')->delete($path);
     }
 }
